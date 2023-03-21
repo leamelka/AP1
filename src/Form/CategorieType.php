c47ccab7c6d515a6ee9e20b405085399f59a09c6
leamelka <106473905+leamelka@users.noreply.github.com>
2023-03-21 21:52:47 +0100

    Create CategorieType.php

---
+++ src/Form/CategorieType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Categorie;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Description of CategorieType
+ *
+ * @author user
+ */
+class CategorieType extends AbstractType
+{
+    /**
+     * Initialisation du formulaire
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+     public function buildForm(FormBuilderInterface $builder, array $options) {
+
+        $builder
+                ->add('name', TextType::class, [
+                    'label' => 'Intitulé de la catégorie',
+                    'required' => true
+                ])
+                ->add('submit', SubmitType::class, [
+                    'label' => 'Ajouter'
+                ])
+        ;   
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Categorie::class,
+        ]);
+    }
+}

b8edcafd265bb43eb567841c1c24a351765dfe80
leamelka <106473905+leamelka@users.noreply.github.com>
2023-03-21 12:41:52 +0100

    M1

---
+++ src/Controller/FormationsController.php
@@ -0,0 +1,118 @@
+<?php
+namespace App\Controller;
+
+use App\Repository\CategorieRepository;
+use App\Repository\FormationRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+define("FORMATION", "pages/formations.html.twig");
+
+/**
+ * Gère les routes de la page des formations
+ *
+ * @author emds
+ */
+class FormationsController extends AbstractController {
+
+    /**
+     * 
+     * @var FormationRepository
+     */
+    private $formationRepository;
+    
+    /**
+     * 
+     * @var CategorieRepository
+     */
+    private $categorieRepository;
+    
+    /**
+     * Création du constructeur
+     * @param FormationRepository $formationRepository
+     * @param CategorieRepository $categorieRepository
+     */
+    function __construct(FormationRepository $formationRepository, CategorieRepository $categorieRepository) {
+        $this->formationRepository = $formationRepository;
+        $this->categorieRepository= $categorieRepository;
+    }
+    
+    /**
+     * Création de la route vers la page des formations
+     * @Route("/formations", name="formations")
+     * @return Response
+     */
+    public function index(): Response{
+        $formations = $this->formationRepository->findAll();
+        $categories = $this->categorieRepository->findAll();
+        return $this->render("pages/formations.html.twig", [
+            'formations' => $formations,
+            'categories' => $categories
+        ]);
+    }
+
+    /**
+     * Tri les enregistrements selon le $champ et l'ordre
+     * Et sur le $champ et l'ordre si autre $table
+     * @Route("/formations/tri/{champ}/{ordre}/{table}", name="formations.sort")
+     * @param type $champ
+     * @param type $ordre
+     * @param type $table
+     * @return Response
+     */
+    public function sort($champ, $ordre, $table=""): Response{
+        if($table !=""){
+            $formations = $this->formationRepository->findAllOrderByTable($champ, $ordre, $table);
+        }else 
+        {
+            $formations = $this->formationRepository->findAllOrderBy($champ, $ordre);
+        }
+        $categories = $this->categorieRepository->findAll();
+        return $this->render(FORMATION, [
+            'formations' => $formations,
+            'categories' => $categories
+        ]);
+    }     
+    
+    /**
+     * Récupère les enregistrements selon le $champ et la $valeur
+     * Et selon le $champ et la $valeur si autre $table
+     * @Route("/formations/recherche/{champ}/{table}", name="formations.findallcontain")
+     * @param type $champ
+     * @param Request $request
+     * @param type $table
+     * @return Response
+     */
+    public function findAllContain($champ, Request $request, $table=""): Response{
+        $valeur = $request->get("recherche");
+        if($table !=""){
+            $formations = $this->formationRepository->findByContainValueTable($champ, $valeur, $table);
+        }else{
+            $formations = $this->formationRepository->findByContainValue($champ, $valeur);
+        }
+        $categories = $this->categorieRepository->findAll();
+        return $this->render(FORMATION, [
+            'formations' => $formations,
+            'categories' => $categories,
+            'valeur' => $valeur,
+            'table' => $table
+        ]);
+    }  
+    
+    /**
+     * Récupère les enregistrements des formations individuelles
+     * @Route("/formations/formation/{id}", name="formations.showone")
+     * @param type $id
+     * @return Response
+     */
+    public function showOne($id): Response{
+        $formation = $this->formationRepository->find($id);
+        return $this->render("pages/formation.html.twig", [
+            'formation' => $formation
+        ]);        
+    }   
+    
+}
